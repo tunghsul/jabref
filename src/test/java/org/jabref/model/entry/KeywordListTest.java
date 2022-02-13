7e2bb7fb4bcb7848cb6ef4840cf83ab9522d6156
Nahal Khalkhali <nahalkhalkhali@Nahals-MacBook-Air.local>
2022-02-13 14:56:36 -0800

    KeywordListTest add more Test Cases

--- src/test/java/org/jabref/model/entry/KeywordListTest.java
+++ src/test/java/org/jabref/model/entry/KeywordListTest.java
@@ -3,6 +3,9 @@ package org.jabref.model.entry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KeywordListTest {
@@ -16,10 +19,119 @@ public class KeywordListTest {
         keywords.add("keywordTwo");
     }
 
+    //Part 3: Our New Test Cases
+
+    @Test
+    public void parseListReturnListTest()throws Exception{
+        List<String> keywordChains = new ArrayList<>();
+        keywordChains.add("key1");
+        keywordChains.add("key2");
+        assertEquals(new KeywordList(keywordChains),
+                KeywordList.parse("key1, key2", ',') );
+    }
+
+    @Test
+    public void createCloneCronstructorTest() throws Exception{
+        KeywordList ky = new KeywordList("key1");
+        assertEquals(ky, ky.createClone());
+    }
+
+    @Test
+    public void replaceAllTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        KeywordList ky_replace = new KeywordList("key2");
+        Keyword ky_new = new Keyword("key4");
+        KeywordList ky2 = new KeywordList("key1", "key4", "key3");
+        ky.replaceAll(ky_replace, ky_new);
+        assertEquals(ky2,ky);
+    }
+
+    @Test
+    public void replaceAllTest2() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        KeywordList ky_replace = new KeywordList("key5");
+        Keyword ky_new = new Keyword("key4");
+        KeywordList ky2 = new KeywordList("key1", "key2", "key3", "key4");
+        ky.replaceAll(ky_replace, ky_new);
+        assertEquals(ky2,ky);
+    }
+
+    @Test
+    public void removeAllTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        KeywordList ky_remove = new KeywordList("key2");
+        KeywordList ky_new = new KeywordList("key1", "key3");
+        ky.removeAll(ky_remove);
+        assertEquals(ky_new, ky);
+    }
+
+//    @Test
+//    public void iteratorTest() throws Exception{
+//        KeywordList ky = new KeywordList("key1");
+//        Array<Keyword> result = new Iterator<>();
+//        result.add(new Keyword("key1"));
+//        assertEquals(result,ky.iterator());
+//    }
+
+    @Test
+    public void sizeTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        assertEquals(3, ky.size());
+    }
+
+    @Test
+    public void isEmptyTest() throws Exception {
+        KeywordList ky = new KeywordList();
+        assertEquals(true, ky.isEmpty());
+    }
+
+    @Test
+    public void containsTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        assertEquals(true, ky.contains("key2"));
+    }
+
+    @Test
+    public void removeKeywordTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        assertEquals(true, ky.remove(new Keyword("key2")));
+    }
+
+    @Test
+    public void removeKeywordStringTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        assertEquals(true, ky.remove("key2"));
+    }
+
+    @Test
+    public void addAllTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        ky.addAll(new KeywordList("key4", "key5"));
+        assertEquals(new KeywordList("key1", "key2", "key3","key4", "key5"),
+                ky);
+    }
+
+    @Test
+    public void clearTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        ky.clear();
+        assertEquals(new KeywordList(),ky);
+    }
+
+    @Test
+    public void getTest() throws Exception {
+        KeywordList ky = new KeywordList("key1", "key2", "key3");
+        assertEquals(new Keyword("key2"), ky.get(1));
+    }
+
+
+
+
     @Test
     public void parseEmptyStringReturnsEmptyList() throws Exception {
         assertEquals(new KeywordList(), KeywordList.parse("", ','));
     }
+
 
     @Test
     public void parseOneWordReturnsOneKeyword() throws Exception {
